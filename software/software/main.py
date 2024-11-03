1156faad315a9bb261b2c0928c1bbf31f9bc466b
Joey Nelson <joey@joescan.com>
2024-11-03 11:39:17 -0800

    more modes

--- software/software/main.py
+++ software/software/main.py
@@ -4,23 +4,23 @@ import math
 import random
 
 
-mode = 6
-max_mode = 8
+
+max_mode = 12
+mode = max_mode - 1
 counter = 0.0
 delay_time = 100
 direction = 1
 velocity = 0
 location = 0
 smoothed_loc = 0
-step = 1500
-step_change = -1
 c2 = 0
 tw = 0
 prev_tw = 0
 vel_array = [1,2,4,8,16,32,64]
 loc_array = [0,0,0,0,0,0,0]
 rot_count = 0
-timeout = 5000
+timeout = 8000
+countdown = timeout
 timestep = 5
 
 display = bytearray(8)
@@ -47,13 +47,16 @@ while True:
          
     if clicked[buttonA]:
         mode = (mode + 1) % max_mode
-        timeout = 5000
+        countdown = timeout
+        c2 = 0
         print("mode:", mode)
      
-    timeout = timeout - timestep
-    if timeout < 0:
-        mode = random.randint(0,max_mode)
-        timeout = 5000
+    countdown = countdown - timestep
+    if countdown < 0:
+        mode = random.randint(0,max_mode-1)
+        c2 = 0
+        countdown = timeout
+        print("mode:", mode)
         
     if mode == 0:
         ## see what's going on with the touch wheel
@@ -71,26 +74,15 @@ while True:
                 smoothed_loc = (location + smoothed_loc * 3) >> 2 
                 print(smoothed_loc)
                 prev_tw = tw
-                
-              
-        step = step + step_change      
-        if step > 2000 or step < 1000:
-            step_change = -step_change
-            
-        
-        if tw > 0:
-            delay_time = 10 + tw
+                              
         
         for i in range(7):
             loc_array[i] += vel_array[i]
             if loc_array[i] >= 1024:
                 loc_array[i] = loc_array[i] - 1024
                 
-                
-        
-        
-        c2 = c2 + (step >> 3)
-        limit = 2000
+        c2 = c2 + 1
+        limit = 10
         if c2 >= limit:
             
             #print(velocity)
@@ -145,32 +137,102 @@ while True:
                 if random.randint(0,1000) < 10:
                     display[i] = display[i] ^ 1 << j
     elif mode == 4:
+        for i in range(8):
+            display[i] = 0
         c2 = c2 + 1
         phase = (c2 >> 3) % 24
+        phase2 = (-c2 >> 5) % 8
         for i in range(8):
             intensity_vals[i] = 9
             if int(phase / 3) == i:
-                display[i] = 1 << (6 - (phase % 3))
-            else:
-                display[i] = 0x00
+                display[i] = display[i] | 1 << (6 - (phase % 3))
+        for i in range(8):
+            if phase2 == i:
+                display[i] = display[i] | 1
+        
+                  
                 
     elif mode == 5:
         for i in range(8):
-            display[i] = random.getrandbits(7)
-            intensity_vals[i] = random.randint(1,10)
+            display[i] = random.getrandbits(7) & random.getrandbits(7) & random.getrandbits(7) & random.getrandbits(7) & random.getrandbits(7) & random.getrandbits(7)
+            intensity_vals[i] = random.randint(1,10) 
             
     elif mode == 6:
         c2 = c2 + 1
-        phase = (c2 >> 4) % (7 * 8 + 1)
+        phase = (c2 >> 3) % (7+8)
         for i in range(8):
             intensity_vals[i] = 9
             display[i] = 0
             for j in range(7):
-                idx = i*7 + j
-                if phase == (idx * (j+i)) % (7 * 8):
+                if phase == j+i:
                     display[i] = display[i] | 1 << j
-
+                
+    
+    elif mode == 7:
+        c2 = c2 + 1
+        t1 = c2 / 30
+        t2 = t1 - math.pi / 8
+        t3 = t2 - math.pi / 8
+        p1 = int((math.sin(t1) + 1) * 4)
+        p2 = int((math.sin(t2) + 1) * 4)
+        p3 = int((math.sin(t3) + 1) * 4)
+        for i in range(8):
+            if i == p1:
+                display[i] = 0x7f
+                intensity_vals[i] = 15
+            elif i == p2:
+                display[i] = 0x78
+                intensity_vals[i] = 7
+            elif i == p3:
+                display[i] = 0x60
+                intensity_vals[i] = 0
+            else:
+                display[i] = 0x00
         
+    elif mode == 8:
+        c2 = c2 + 1
+        pattern = 0xff00ff
+        for i in range(8):
+            phase = ((c2 >> 4) + i) % 8
+            display[i] = (pattern << phase) & 0x7f
+            intensity_vals[i] = 9            
+     
+    elif mode == 9:
+        step = int((math.sin(c2 / 900) + 2) * 10) 
+        c2 = c2 + step
+        phase = (c2 >> 8) % 4
+        for i in range(8):
+            pd = (phase - i) % 4
+            intensity_vals[i] = min(15,max(0, (4 - pd * 3) * 4))
+            if pd < 3:
+                display[i] = 0x7f
+            else:
+                display[i] = 0x00
+    
+    elif mode == 10:
+        for i in range(8):
+            pattern = 0
+            for j in range(7):
+                if random.randint(0,1000) < 20 * (8 - j):
+                    pattern = pattern | 1 << j
+            display[i] = pattern
+            intensity_vals[i] = random.randint(1,10) 
+    
+    elif mode == 11:
+        c2 = c2 + 1
+        
+        phase = (c2 * 3 >> 5) % 8
+        stepx = (c2 * 5 >> 10) % 16 + 1
+        
+        for i in range(8):
+            intensity_vals[i] = 9
+            display[i] = 0
+            for j in range(7):
+                if (phase + j * 8 + i) % stepx == 0:
+                    display[i] = 1 << j
+            
+                
+                    
             
     for i in range(4):
         base_reg = 0x10
